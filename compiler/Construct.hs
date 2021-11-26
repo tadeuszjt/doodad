abe59b19ac35a755bac2a01e015c2fc0cac84c57
Tadeusz <tadeuszjt@gmail.com>
2021-11-26 22:54:21 +0000

    Added forgotten Construct.hs file

---
+++ compiler/Construct.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE FlexibleContexts #-}
+module Construct where
+
+
+import qualified LLVM.AST.Type as LL
+import LLVM.IRBuilder.Instruction       
+
+import Type
+import State
+import Monad
+import Tuple
+import Value
+import ADT
+
+
+
+valConstruct :: InsCmp CompileState m => Type -> [Value] -> m Value
+valConstruct typ []    = zeroOf typ
+valConstruct typ [val] = do
+    val' <- valResolveContextual val
+
+    if valType val' == typ then do
+        valLoad val'
+    else do
+        val' <- valLoad val
+        base <- baseTypeOf typ
+        case base of
+            Table [Char] -> do
+                ObjFunc retty op <- look "string" (KeyFunc [valType val])
+                Val retty <$> call op [(valOp val', [])]
+
+            I32 -> case val' of
+                Val I64 op -> Val typ <$> trunc op LL.i32
+                Val I8 op  -> Val typ <$> sext op LL.i32
+
+            I64 -> case val' of
+                Val Char op -> Val typ <$> sext op LL.i64
+
+            Char -> case val' of
+                Val I64 op -> Val typ <$> trunc op LL.i8
+                Val I32 op -> Val typ <$> trunc op LL.i8
+                _          -> error (show val')
+
+            ADT _       -> adtConstruct typ val'
+            _           -> do
+                pureType    <- pureTypeOf typ
+                pureValType <- pureTypeOf (valType val')
+                checkTypesMatch pureType pureValType
+                Val typ <$> valOp <$> valLoad val'
+
+valConstruct typ vals = tupleConstruct typ vals
