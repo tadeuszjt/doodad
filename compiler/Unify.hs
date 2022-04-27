2010868667fbf3fec693d7e39e2214ed186830f2
Tadeusz Tomoszek <Tadeusz.Tomoszek@viavisolutions.com>
2022-04-27 23:03:16 +0100

    Added Unify.hs

---
+++ compiler/Unify.hs
@@ -0,0 +1,121 @@
+module Unify where
+
+import Type as T
+import AST as S
+
+
+-- constraint:   (t1, t2) or (x, y)
+-- substitution: (id, t) or (x, u)
+substitute :: Type -> Int -> Type -> Type
+substitute u x t = case t of
+    Type i | i == x  -> u
+    Type _           -> t
+    T.Bool           -> t
+    I64              -> t
+    I32              -> t
+    F32              -> t
+    F64              -> t
+    T.Char           -> t
+    T.Table [T.Char] -> t
+    T.Tuple ts       -> T.Tuple $ map (substitute u x) ts
+    Void             -> t
+    _                -> error (show t)
+
+
+unifyOne :: Type -> Type -> [(Int, Type)]
+unifyOne t1 t2 = case (t1, t2) of
+    (Type x, t)                          -> [(x, t)]
+    (t, Type x)                          -> [(x, t)]
+    (T.Bool, T.Bool)                     -> []
+    (I64, I64)                           -> []
+    (F32, F32)                           -> []
+    (F64, F64)                           -> []
+    (T.Char, T.Char)                     -> []
+    (T.Table [T.Char], T.Table [T.Char]) -> []
+    _                                    -> error $ show (t1, t2)
+
+
+unify :: [(Type, Type)] -> [(Int, Type)]
+unify []          = []
+unify ((x, y):xs) = let t2 = unify xs in
+    unifyOne (apply t2 x) (apply t2 y) ++ t2
+
+
+-- Apply represents taking a list of substitutions and applying them to all types in an object.
+class Apply a where
+    apply :: [(Int, Type)] -> a -> a
+
+instance Apply Type where
+    apply subs t = foldr (\(x, u) z -> substitute u x z) t subs
+    
+instance Apply Expr where
+    apply subs expr = case expr of
+        AExpr t e                -> AExpr (apply subs t) (apply subs e)
+        Infix pos op expr1 expr2 -> Infix pos op (apply subs expr1) (apply subs expr2)
+        S.Tuple pos exprs        -> S.Tuple pos $ map (apply subs) exprs
+        Ident pos sym            -> expr
+        S.Char  pos c            -> expr
+        Int   pos n              -> expr
+        Prefix pos op expr1      -> Prefix pos op (apply subs expr1)
+        Call  pos sym exprs      -> Call pos sym $ map (apply subs) exprs
+        Conv  pos t exprs        -> Conv pos (apply subs t) $ map (apply subs) exprs
+        Copy  pos e              -> Copy pos (apply subs e)
+        Len   pos e              -> Len  pos (apply subs e)
+        S.Bool  pos b            -> expr
+        Subscript pos e1 e2      -> Subscript pos (apply subs e1) (apply subs e2)
+        String pos s             -> expr
+        Range pos e ml mr        -> Range pos (apply subs e) (fmap (apply subs) ml) (fmap (apply subs) mr)
+        _                          -> error $ show expr
+
+instance Apply Condition where
+    apply subs cnd = case cnd of
+        CondExpr expr -> CondExpr (apply subs expr)
+        _               -> error $ show cnd
+
+instance Apply Pattern where
+    apply subs pattern = case pattern of
+        PatIdent p s       -> pattern
+        PatLiteral e       -> PatLiteral (apply subs e)
+        PatGuarded p pat e -> PatGuarded p (apply subs pat) (apply subs e)
+        _                    -> error $ show pattern
+
+instance Apply Append where
+    apply subs app = case app of
+        _ -> error $ show app
+
+instance Apply Index where
+    apply subs index = case index of
+        IndIdent p sym -> index
+        IndArray p ind e -> IndArray p (apply subs ind) (apply subs e)
+        _ -> error $ show index
+
+instance Apply Stmt where
+    apply subs stmt = case stmt of
+        Block stmts             -> Block $ map (apply subs) stmts
+        Return pos mexpr        -> Return pos $ fmap (apply subs) mexpr
+        Assign pos pat expr     -> Assign pos (apply subs pat) (apply subs expr)
+        AppendStmt app          -> AppendStmt (apply subs app)
+        For pos idxStr e em blk -> For pos idxStr (apply subs e) (fmap (apply subs) em) (apply subs blk)
+        Set pos index e         -> Set pos (apply subs index) (apply subs e)
+        While pos cnd blk       -> While pos (apply subs cnd) (apply subs blk)
+        CallStmt pos sym es     -> CallStmt pos sym $ map (apply subs) es
+
+        FuncDef pos sym params retty block ->
+            let params' = map (\(Param p s t) -> Param p s (apply subs t)) params in
+            FuncDef pos sym params' (apply subs retty) (apply subs block)
+
+        Extern pos name sym params retty ->
+            let params' = map (\(Param p s t) -> Param p s (apply subs t)) params in
+            Extern pos name sym params' (apply subs retty)
+
+        If pos cnd block melse ->
+            If pos (apply subs cnd) (apply subs block) $ fmap (apply subs) melse
+
+        Switch pos e cases ->
+            let cases' = map (\(pat, stmt) -> (apply subs pat, apply subs stmt)) cases in
+            Switch pos (apply subs e) cases'
+
+        _ -> error $ show stmt
+
+instance Apply AST where
+    apply subs ast = ast { astStmts = map (apply subs) (astStmts ast) }
